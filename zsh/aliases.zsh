f6198de5bb1f2982aa056966084ccd20395ea977
Eli <88557639+lishaduck@users.noreply.github.com>
2024-12-05 22:32:12 -0600

    chore: run shfmt

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,18 +1,18 @@
 # https://opensource.com/article/19/7/bash-aliases
-alias lt='du -sh \* | sort -h';
-alias count='find . -type f | wc -l';
+alias lt='du -sh \* | sort -h'
+alias count='find . -type f | wc -l'
 
 # https://www.webpro.nl/articles/getting-started-with-dotfiles
 # Recursively remove .DS_Store files
-alias cleanupds="find . -type f -name '\*.DS_Store' -ls -delete";
+alias cleanupds="find . -type f -name '\*.DS_Store' -ls -delete"
 
 # Alias to launch chrome from terminal
-alias chrome='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome';
+alias chrome='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
 
 # Aliases for 'eza'
 alias _eza_base='eza --classify --hyperlink --header --group-directories-first -I .git --icons --git --git-ignore --no-permissions'
 alias eza_long='_eza_base --long --all --dereference --total-size'
-alias eza_tree='_eza_base --tree --almost-all --git';
+alias eza_tree='_eza_base --tree --almost-all --git'
 
 # Aliases for brew
 alias brewgraph='brew graph --installed --highlight-leaves | fdp -T png -o graph.png'
