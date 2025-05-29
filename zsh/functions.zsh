68dc0d10409fdc2a2d50a938045e0d050482caa0
Eli <88557639+lishaduck@users.noreply.github.com>
2025-05-29 15:38:51 -0500

    feat: prettify backup checker

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -4,16 +4,29 @@ function mk() {
   mkdir -p "$@" && cd "$@" || exit
 }
 
-function check_for_dirty() {
+function check_backups() {
   local dir=${1:-"$HOME/Developer"}
-  find "$dir" -maxdepth 2 -type d -name '.git' -execdir sh -c '
-    git fetch &> /dev/null &&
-    ( [[ -n $(git status --porcelain) ]] ||
-      [[ -n $(git rev-list HEAD@{upstream}..HEAD) ]] ) &&
-    pwd' \;
-}
 
-function check_for_gitless() {
-  local dir=${1:-"$HOME/Developer"}
-  find "$dir" -maxdepth 1 -type d ! -exec test -d '{}/.git' \; -print
+  find "$dir" -mindepth 1 -maxdepth 1 -type d | while read -r subdir; do
+    if [[ -d "$subdir/.git" ]]; then
+      echo -e "\033[3;34m$subdir\033[0m"
+      # It's a git repo
+      git -C "$subdir" fetch &> /dev/null
+      dirty=$(git -C "$subdir" status --porcelain)
+      upstream=$(git -C "$subdir" rev-parse --abbrev-ref --symbolic-full-name @{u} 2>/dev/null)
+      echo -en "\033[1A"
+      if [[ -n "$dirty" ]] || { [[ -n "$upstream" ]] && [[ -n $(git -C "$subdir" rev-list @{u}..HEAD 2>/dev/null) ]]; }; then
+        # Bold yellow if dirty or unpushed
+        echo -e "\033[1;33m$subdir $(starship module git_status --path=$subdir)\033[0m"
+      else
+        # Italic grey if clean
+        echo -e "\033[3;90m$subdir\033[0m"
+      fi
+    elif [[ -d "$subdir/.jj" ]] then;
+      echo -e "\033[36m$subdir (Jujutsu)\033[0m"
+    else
+      # Not a git repo: bold red
+      echo -e "\033[1;31m$subdir (no repo)\033[0m"
+    fi
+  done
 }
